8ca1cee8f0b7286f37f5184eefb91de4c1c10ff9
= <danishfaisal.wwe@gmail.com>
2021-11-20 12:58:09 +0530

    Creating React Elements

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  React.createElement('h1', { style: { color: 'blue' } }, 'Heyyy Everyone!'),
   document.getElementById('root')
 );
 
