6029a4d70c9a2ca2f112e40d0ecd3c89e4ff4406
= <danishfaisal.wwe@gmail.com>
2021-12-11 12:07:33 +0530

    Adding component properties

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header() {
+function Header(props) {
   return (
-    <h1>Eve's Kitchen</h1>
+    <h1>{props.name}'s Kitchen</h1>
   );
 }
 
-function Main() {
+function Main(props) {
   return (
     <section>
-      <p>We serve the most delicious food around.</p>
+      <p>We serve the most {props.adjective} food around.</p>
     </section>
   );
 }
 
-function Footer() {
+function Footer(props) {
   return (
     <footer>
-      <p>It's true.</p>
+      <p>Copyright {props.year}</p>
     </footer>
   );
 }
@@ -26,9 +26,9 @@ function Footer() {
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <Header name="Cindy" />
+      <Main adjective="amazing" />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
